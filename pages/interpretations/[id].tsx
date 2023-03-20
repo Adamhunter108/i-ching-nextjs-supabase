380f83a1cc4e46cf4df52d3f950a3c3cca82b4d2
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2023-03-20 17:11:15 -0400

    Update [id].tsx

--- pages/interpretations/[id].tsx
+++ pages/interpretations/[id].tsx
@@ -2,15 +2,14 @@ import React from 'react'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 import Head from 'next/head'
+import Link from 'next/link'
 import HexInterpretations from '@/data/HexInterpretations'
 import Nav from '@/components/Nav'
 
-export default function Interpretations(props: any) {
+export default function Interpretations() {
     const router = useRouter()
     const routerID = router.query.id
-    console.log(typeof routerID)
     const routerIDNum = parseInt(routerID as string, 10)
-    console.log(typeof routerIDNum)
 
   return (
     <div>
@@ -24,6 +23,19 @@ export default function Interpretations(props: any) {
             <svg className="animate-spin-slow w-16 h-16" aria-hidden="true" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                 <path d="M256 128C238.3 128 224 142.4 224 160S238.3 192 256 192s31.97-14.38 31.97-32S273.7 128 256 128zM256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 384c-17.68 0-31.97-14.38-31.97-32S238.3 320 256 320s31.97 14.38 31.97 32S273.7 384 256 384zM256 256c-53.04 0-96.03 43-96.03 96S202.1 448 256 448c-106.1 0-192.1-86-192.1-192S149.9 64 256 64c53.04 0 96.03 43 96.03 96S309 256 256 256z"/>
             </svg>
+        </div>
+
+        <div className="pt-2 lg:pt-5 pl-3 lg:pl-72">
+        <Link href="/profile">
+            <button
+                className="lg:fixed lg:z-50 px-3.5 mt-8 py-2 bg-gradient-to-b from-indigo-800 via-indigo-600 to-indigo-400 text-cyan-100 hover:bg-gradient-to-t from-indigo-800 via-indigo-600 to-indigo-400 font-light rounded-md text-lg flex flex-row items-center"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+                </svg>
+                <span className="pl-1">Back</span>
+            </button>
+        </Link>
         </div>
 
 
@@ -48,7 +60,6 @@ export default function Interpretations(props: any) {
 
                     <p className="flex justify-center text-lg">{routerIDNum === data.id ? `Hexagram # ${routerIDNum}` : null}</p>
 
-
                     <div className="flex justify-center">
                         <p className="text-center font-bold text-4xl text-transparent bg-clip-text bg-gradient-to-b from-indigo-200 to-teal-200">{routerIDNum === data.id ? data.title : null}</p>
                     </div>
@@ -61,34 +72,16 @@ export default function Interpretations(props: any) {
             ))}
         </ul>
 
+        <div className="group mt-10 flex justify-center">
+            {/* <Link href={`https://www.google.com/search?q=i+ching+hexagram+${routerID}`} target="_blank"> */}
+            <Link href={`https://letmegooglethat.com/?q=i+ching+hexagram+${routerID}`} target="_blank">
+                <p className="text-cyan-200 hover:underline">Deep dive...</p>
+                <span className="absolute scale-0 transition-all rounded bg-gray-900 p-1 text-xs text-cyan-100 group-hover:scale-100">
+                    Let me Google that for you <span className="text-lg">😎</span>
+                </span>
+            </Link>
+        </div>
+
     </div>
   )
 }
-
-
-// not sure if I need these...
-// // static site generation (or serverside) needs to get data before page loads
-// export async function getStaticProps({params}) {
-    // GET HEX INTERPREDATIONS FROM @DATA
-    
-//     return {
-//         props: {
-//             // define props
-//         },
-
-//         revalidate: 60,
-//     }
-// }
-
-// // https://nextjs.org/docs/basic-features/data-fetching/get-static-paths
-// export async function getStaticPaths() {
-
-//     const paths = HexInterpretations.map((data) => {
-//         return {params: {id: data.id.toString()}}
-//     })
-    
-//     return {
-//       paths,
-//       fallback: false
-//     };
-//   }
